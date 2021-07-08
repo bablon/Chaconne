fb329bf1291b710645c0e3ceb12fb7c22c3fa9ee
Jiajia Liu <liujia6264@gmail.com>
2021-07-08 11:10:54 +0800

    fix cpuid and add -ecx option

--- cpuid.c
+++ cpuid.c
@@ -7,34 +7,39 @@
 #include "hashtable.h"
 
 COMMAND(cmd_cpuid, NULL,
-	"cpuid {-eax UINT}",
+	"cpuid {-eax UINT|-ecx UINT}",
 	"cpuid command\n"
 	"eax option for cpuid\n"
 	"specify eax value\n"
+	"ecx option for cpuid\n"
+	"specify ecx value\n"
 	)
 {
 	struct cpuidreg id;
 	const char *eaxstr = hashtable_get(opt->kpairs, "-eax");
-	uint32_t eax;
+	const char *ecxstr = hashtable_get(opt->kpairs, "-ecx");
+	uint32_t eax, ecx = 0;
 
 	if (!eaxstr) {
-		do_cpuid(&id, 0);
+		do_cpuid(&id, 0, 0);
 		term_print(term, "%.*s%.*s%.*s, ",
 			4, (char *)&id.ebx, 4, (char *)&id.edx, 4, (char *)&id.ecx);
-		do_cpuid(&id, 0x80000002);
+		do_cpuid(&id, 0x80000002, 0);
 		term_print(term, "%.*s%.*s%.*s%.*s", 4, (char *)&id.eax,
 			4, (char *)&id.ebx, 4, (char *)&id.ecx, 4, (char *)&id.edx);
-		do_cpuid(&id, 0x80000003);
+		do_cpuid(&id, 0x80000003, 0);
 		term_print(term, "%.*s%.*s%.*s%.*s", 4, (char *)&id.eax,
 			4, (char *)&id.ebx, 4, (char *)&id.ecx, 4, (char *)&id.edx);
-		do_cpuid(&id, 0x80000004);
+		do_cpuid(&id, 0x80000004, 0);
 		term_print(term, "%.*s%.*s%.*s%.*s.\r\n", 4, (char *)&id.eax,
 			4, (char *)&id.ebx, 4, (char *)&id.ecx, 4, (char *)&id.edx);
 		return 0;
 	}
 
 	eax = strtoul(eaxstr, NULL, 0);
-	do_cpuid(&id, eax);
+	if (ecxstr)
+		ecx = strtoul(ecxstr, NULL, 0);
+	do_cpuid(&id, eax, ecx);
 
 	if (eax == 0) {
 		term_print(term, "eax 0x%x, ID %.*s%.*s%.*s.\r\n", id.eax,
--- cpuid.h
+++ cpuid.h
@@ -10,14 +10,15 @@ struct cpuidreg {
 static inline void cpuid(uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx)
 {
 	asm volatile ("cpuid"
-		      : "+a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx));
+		      : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
+		      : "0" (*eax), "2" (*ecx) : "memory");
 }
 
-static inline void do_cpuid(struct cpuidreg *id, uint32_t eax)
+static inline void do_cpuid(struct cpuidreg *id, uint32_t eax, uint32_t ecx)
 {
 	id->eax = eax;
 	id->ebx = 0;
-	id->ecx = 0;
+	id->ecx = ecx;
 	id->edx = 0;
 
 	cpuid(&id->eax, &id->ebx, &id->ecx, &id->edx);
